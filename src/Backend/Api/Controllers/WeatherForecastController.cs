51972600b3ff4ea1d774aca421836e4f683ee0e8
matheus.nsantos <matheus.nsantos@boavistascpc.com.br>
2024-06-25 11:06:42 -0300

    add serilog

--- src/Backend/Api/Controllers/WeatherForecastController.cs
+++ src/Backend/Api/Controllers/WeatherForecastController.cs
@@ -11,17 +11,19 @@ namespace Api.Controllers
 			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 		};
 
-		private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<WeatherForecastController> _logger;
 
-		public WeatherForecastController(ILogger<WeatherForecastController> logger)
-		{
-			_logger = logger;
-		}
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
 
-		[HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetWeatherForecast")]
 		public IEnumerable<WeatherForecast> Get()
 		{
-			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            _logger.LogInformation("Requesting Weather Forecast Details...");
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
 			{
 				Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
 				TemperatureC = Random.Shared.Next(-20, 55),
--- src/Backend/Api/Program.cs
+++ src/Backend/Api/Program.cs
@@ -1,4 +1,5 @@
 using Infrastructure.IoC;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,11 @@ builder.Services.AddSwaggerGen();
 //DI
 builder.Services.AddInfrastructure(builder.Configuration);
 
+//Serilog
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -16,6 +22,8 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
